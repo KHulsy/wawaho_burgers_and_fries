f3ffaf776e9a19a8fb8a72e8d998414f0e19c3fc
KHulsy <66654951+KHulsy@users.noreply.github.com>
2020-07-25 16:04:05 -0700

    Create burgers.sql
    SQL Workbench is Read-Only, and therefore can't save file? Okay. Copy/paste.

---
+++ burgers.sql
@@ -0,0 +1,6 @@
+INSERT INTO 'wawahoburgers'('id','wawaho','burgers','burgers eaten')VALUES('NULL', 'wawaho', 'the notdog', '5');
+INSERT INTO 'wawahoburgers'('id', 'wawaho','burgers', 'burgers eaten')VALUES('NULL', 'wawaho', 'theursulaburger', '12');
+INSERT INTO 'wawahoburgers'('id', 'wawaho', 'burgers', 'burgers eaten')VALUES('NULL', 'wawaho', 'the iwantamuffin', '10');
+INSERT INTO 'wawahoburgers'('id', 'wawaho', 'burgers', 'burgers eaten')VALUES('NULL', 'wawaho', 'the youaresopretty', '6');
+INSERT INTO 'wawahoburgers'('id', 'wawaho', 'burgers', 'burgers eaten')VALUES('NULL', 'wawaho', 'the wannawatchbambi'),15);
+INSERT INTO 'wawahoburgers'('id', 'wawaho', 'burgers', 'burgers eaten')VALUES('NULL', 'wawaho', 'thewawahoburger',8);
